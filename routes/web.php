97ee228847cb35a8106f2009c652c9323a12b808
krol1ck <aaw1812.slutulaur@gmail.com>
2023-11-08 12:36:21 +0200

    modificari route

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return redirect()->route('login');
+    if (Auth::check()) {
+        return redirect()->route('chatify');
+    } else {
+        return redirect()->route('login');
+    }
 });
 
 Route::middleware([
@@ -24,5 +28,5 @@ Route::middleware([
 ])->group(function () {
     Route::get('/dashboard', function () {
         return redirect()->route('chatify');
-    });
+    })->name('dashboard');
 });
